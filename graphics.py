e9edb330591a98b5affc99ace31621722c934a16
cekapitan <162815519+cekapitan@users.noreply.github.com>
2024-12-28 00:22:05 -0500

    taking off of here for a min

--- graphics.py
+++ graphics.py
@@ -9,6 +9,7 @@ class Window:
         self.__root = Tk()
         self.__root.title("Maze_Solver")
         self.__canvas = Canvas(self.__root, bg = "white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE", self.close)
 
@@ -22,5 +23,21 @@ class Window:
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def close(self) -> None:
         self.__is_running = False
+    
+class Point:
+    def __init__(self, x : int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1 : int, p2: int) -> None:
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color="black") -> None:
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(90,59), Point(900,400))
+    win.draw_line(l,"black")
+
     win.wait_for_close()
 
 
